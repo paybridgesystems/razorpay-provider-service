feda972a874b1f94bbfa25811c3e7b6e039c7547
smaulik557-admin <smaulik557@gmail.com>
2026-06-05 18:32:42 +0530

    Remove hardcoded values across the project and add it to a Constant file

---
+++ src/main/java/com/paybridge/payments/constant/Constant.java
@@ -0,0 +1,16 @@
+package com.paybridge.payments.constant;
+
+public class Constant {
+	private Constant() {}
+	
+	public static final String RAZORPAY_CLIENT = "razorpay-client";
+	
+	public static final int CONVERT_TO_PAISE = 100;
+	
+	public static final String DEFAULT_CURRENCY = "INR";
+	
+	public static final String RECEIPT_ID_TEMPLATE = "receipt_id_";
+	
+	public static final int PAYMENT_CAPTURE = 0;
+	
+}
