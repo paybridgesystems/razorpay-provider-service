20d9a74d498f8e0ab9f33bea003ddb44392bcbd5
smaulik557-admin <smaulik557@gmail.com>
2026-06-18 19:25:16 +0530

    Refactor: Removed default currency related logic

--- src/main/java/com/paybridge/payments/service/OrderService.java
+++ src/main/java/com/paybridge/payments/service/OrderService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import com.paybridge.payments.client.RazorpayClient;
 import com.paybridge.payments.client.model.RazorpayOrderRequest;
 import com.paybridge.payments.client.model.RazorpayOrderResponse;
-import com.paybridge.payments.constant.Constant;
+import com.paybridge.payments.constant.RazorpayConstants;
 import com.paybridge.payments.dto.OrderRequest;
 import com.paybridge.payments.dto.OrderResponse;
 import com.paybridge.payments.service.helper.UniqueIdGenerator;
@@ -18,38 +18,38 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 public class OrderService {
-	
+
 	private final RazorpayClient razorpayClient;
-	
+
 	private final UniqueIdGenerator uniqueIdGenerator;
 
 	public ResponseEntity<OrderResponse> createOrder(OrderRequest orderRequest) throws RuntimeException {
 		log.info("createOrder called in OrderService with orderRequest: {}", orderRequest);
-		
+
 		RazorpayOrderRequest razorpayRequest = 
 				RazorpayOrderRequest.builder()
-		                        .amount(orderRequest.getAmount() * Constant.RUPEE_CONVERTION)    // RazorPay expects amount in paise, so multiply by 100		
-		                        .currency(orderRequest.getCurrency() != null ? orderRequest.getCurrency() : Constant.DEFAULT_CURRENCY)
-		                        .receipt(Constant.RECEIPT_ID_TEMPLATE + uniqueIdGenerator.generateUniqueId())
-		                        .paymentCapture(Constant.PAYMENT_CAPTURE)    
-		                        .build();
-		
+				.amount(orderRequest.getAmount() * RazorpayConstants.RUPEE_CONVERSION_FACTOR)		
+				.currency(orderRequest.getCurrency())
+				.receipt(RazorpayConstants.RECEIPT_ID_PREFIX + uniqueIdGenerator.generateUniqueId())
+				.paymentCapture(RazorpayConstants.PAYMENT_CAPTURE_DISABLED)    
+				.build();
+
 		try {
 			log.info("Calling RazorpayClient with request: {}", razorpayRequest);
-	        RazorpayOrderResponse response = razorpayClient.createOrder(razorpayRequest);
-	        
-	        log.info("Order created successfully with Razorpay, response: {}", response);
-	        return ResponseEntity.ok(OrderResponse.builder()
-	        		.amount(response.getAmount() / Constant.RUPEE_CONVERTION)
-	        		.currency(response.getCurrency())
-	        		.orderId(response.getId())
-	        		.receipt(response.getReceipt())
-	        		.rpStatus(response.getStatus())
-	        		.build());
-	        
-	    } catch (Exception x) {
-	    	log.error("Unexpected error while calling Razorpay", x);
-	        throw new RuntimeException("Failed to create order with Razorpay", x);
-	    }
+			RazorpayOrderResponse response = razorpayClient.createOrder(razorpayRequest);
+
+			log.info("Order created successfully with Razorpay, response: {}", response);
+			return ResponseEntity.ok(OrderResponse.builder()
+					.amount(response.getAmount() / RazorpayConstants.RUPEE_CONVERSION_FACTOR)
+					.currency(response.getCurrency())
+					.orderId(response.getId())
+					.receipt(response.getReceipt())
+					.rpStatus(response.getStatus())
+					.build());
+
+		} catch (Exception x) {
+			log.error("Unexpected error while calling Razorpay", x);
+			throw new RuntimeException("Failed to create order with Razorpay", x);
+		}
 	}
 }
